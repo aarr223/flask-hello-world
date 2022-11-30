c3ba11e7c1c1457e3d728cbc26ee8db2aefa1493
aarr223 <119383386+aarr223@users.noreply.github.com>
2022-11-30 14:26:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
+import discord
 from flask import Flask
 app = Flask(__name__)
+
+
+TOKEN = 'MTA0NDI0Mjc1NTQ5NDM1NDk4Ng.GmdmOt.dGuvst2LK00EcmVjZGOmmaeE9PNosbYckDK8W4'
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    if message.content == '/neko':
+        await message.channel.send('Send')
+client.run(TOKEN)
 
 @app.route('/')
 def hello_world():
